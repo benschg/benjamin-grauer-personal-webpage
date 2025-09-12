77f26b42340c30e9170a065df8ddaf58aa871b4a
Benjamin Grauer <grauerb@gmail.com>
2025-09-12 21:44:49 +0200

    fix yarn

--- frontend/src/pages/WorkingLife/types/DocumentTypes.ts
+++ frontend/src/pages/WorkingLife/types/DocumentTypes.ts
@@ -1,7 +1,9 @@
+import type { SvgIconProps } from '@mui/material/SvgIcon';
+
 export interface Document {
   title: string;
   description: string;
-  icon: React.ReactNode;
+  icon: React.ComponentType<SvgIconProps>;
   downloadUrl: string;
   downloadAs: string;
   fileType: string;
