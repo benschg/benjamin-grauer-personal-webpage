e4800ce3c1533ea54d1e6158f76fd611704bf679
Benjamin Grauer <grauerb@gmail.com>
2025-09-12 10:54:33 +0200

    Added font loading, so we do not have any jumping on the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,22 @@ import Home from './pages/Home';
 import WorkingLife from './pages/WorkingLife';
 import PersonalLife from './pages/PersonalLife';
 import Portfolio from './pages/Portfolio';
+import FontLoader from './components/common/FontLoader';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/working-life" element={<WorkingLife />} />
-          <Route path="/personal-life" element={<PersonalLife />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-      </div>
-    </Router>
+    <FontLoader>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/working-life" element={<WorkingLife />} />
+            <Route path="/personal-life" element={<PersonalLife />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </div>
+      </Router>
+    </FontLoader>
   );
 }
 
